c5a79a410d4892b7df0d5b3d2aa4bee7632ee6ad
Carlos <cabueno10@misena.edu.co>
2021-05-28 15:57:50 -0500

    deployment config added

--- deployment-config/scripts/install_dependencies.sh
+++ deployment-config/scripts/install_dependencies.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 # change permissions
 sudo chown -R ec2-user /home/ec2-user/app-backend
 

22f4d0056c3ee895582c0961358dd45084d07068
Carlos <cabueno10@misena.edu.co>
2021-05-28 15:51:06 -0500

    deployment config added

--- deployment-config/scripts/stop_server.sh
+++ deployment-config/scripts/stop_server.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 #stop server
+(cd /home/ec2-user/app-backend && nvm install)
 pm2 stop all || true
 pm2 delete app-backend || true
 
